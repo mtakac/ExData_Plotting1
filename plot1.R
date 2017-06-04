4ed8990aa9945716d94a980874163112deae3b4c
Marek Takac <marek.takac@nejremeslnici.cz>
2017-06-04 23:12:23 +0200

    Refactor plot1 function

--- plot1.R
+++ plot1.R
@@ -3,29 +3,22 @@ source("read_data.R")
 
 ## Create plot1 and write it into the png file
 ## Args: None
-## Return: Histogram
+## Return: None
 plot1 <- function() {
     file_path <- download();
     df <- read_data(file_path);
 
     ## Filter data for first two days in February
     print("Filtering data...")
-    df <- filter(df, Date == "2007-02-01" | Date == "2007-02-02")
+    df <- filter(df, Date == "1/2/2007" | Date == "1/2/2007")
     print("Done!")
 
     ## Open connection to png graphic device
     print("Writting graph into file './plot.png'...")
     png(filename = "./plot1.png")
     ## Plot data into graph
-    graph <- hist(
-        df_feb$Global_active_power,
-        col = "red",
-        main = "Global Active Power",
-        xlab = "Global Active Power (kilowatts)"
-    )
+    hist(df_feb$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
     ## Close connection to png graphic device
     dev.off()
     print("Done!")
-
-    return(graph)
 }
